45482373ef96d6f5c0206bc9605860f1ccc0ff88
pol-robledillo <pol.robledillo.7e7@itb.cat>
2024-01-18 20:02:03 +0100

    Added validate attempts function.

--- LlibreriaJoc/Class1.cs
+++ LlibreriaJoc/Class1.cs
@@ -1,7 +1,14 @@
 ﻿namespace LlibreriaJoc
 {
-    public class Class1
+    public class GlobalMethods
     {
-
+        public static bool ValidateOption(string option, string[] validOptions)
+        {
+            return validOptions.Contains(option);
+        }
+        public static bool ValidateAttempts(int attempts)
+        {
+            return attempts > 0;
+        }
     }
 }
